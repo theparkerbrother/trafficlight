170e5d66b576899945f5e45835bf41a1ed3af4e1
theparkerbrother <forrest273@gmail.com>
2025-01-24 18:49:44 -0700

    completed project with base requirements

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,47 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	
+	const [selectedLight, setSelectedLight] = useState("red");
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working... my change - Forrest
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const getBoxShadow = (color) => {
+		switch (color) {
+		  case "red":
+			return "0 0px 20px rgba(61, 0, 0, 0.8), 0 0px 40px rgba(61, 0, 0, 0.5)";
+		  case "yellow":
+			return "0 0px 20px rgba(102, 102, 0, 0.8), 0 0px 40px rgba(102, 102, 0, 0.5)";
+		  case "green":
+			return "0 0px 20px rgba(29, 73, 9, 0.8), 0 0px 40px rgba(29, 73, 9, 0.5)";
+		  default:
+			return "0 0px 20px rgba(255, 255, 255, 0.8), 0 0px 40px rgba(255, 255, 255, 0.5)";
+		}
+	  };
+
+	return (
+		<div className="d-flex flex-column align-items-center">
+			<div id="trafficTop"></div>
+			<div id="lightContainer" className="d-flex flex-column align-items-center justify-content-around">
+			<div
+				className={`red light ${selectedLight === "red" ? "selected" : ""}`}
+				onClick={() => setSelectedLight("red")}
+				style={{ boxShadow: selectedLight === "red" ? getBoxShadow("red") : "" }}
+			></div>
+			<div
+				className={`yellow light ${selectedLight === "yellow" ? "selected" : ""}`}
+				onClick={() => setSelectedLight("yellow")}
+				style={{ boxShadow: selectedLight === "yellow" ? getBoxShadow("yellow") : "" }}
+			></div>
+			<div
+				className={`green light ${selectedLight === "green" ? "selected" : ""}`}
+				onClick={() => setSelectedLight("green")}
+				style={{ boxShadow: selectedLight === "green" ? getBoxShadow("green") : "" }}
+			></div>
+			</div>
 		</div>
 	);
 };
